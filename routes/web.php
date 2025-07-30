e03b75fbe3b4f5c7817e1def883c2be56aac70b6
Magdiel Castillo <magdielmagdiel1@gmail.com>
2025-07-30 09:47:12 -0400

    ajuste de las rutas para mas legibilidad

--- routes/web.php
+++ routes/web.php
@@ -6,14 +6,11 @@ use App\Http\Controllers\LotoLeidsaController;
 
 Route::get("/", [LoteriaController::class,"index"]) -> name("index");
 
-Route::get("/loto", [LotoLeidsaController::class,"index"]) -> name("loto.index");
-
-Route::get("/loto/agregar", [LotoLeidsaController::class,"create"])->name("loto.agregar");
-
-Route::post("/loto/store", [LotoLeidsaController::class,"store"])->name("loto.store");
-
-Route::get("/loto/editar/{lotteryResult}", [LotoLeidsaController::class,"edit"])->name("loto.editar");
-
-Route::put("/loto/update/{lotteryResult}", [LotoLeidsaController::class,"update"])->name("loto.update");
-
-Route::delete("/loto/delete/{lotteryResult}", [LotoLeidsaController::class,"destroy"]) -> name("loto.delete");
+Route::prefix('loto')->name('loto.')->group(function() {
+    Route::get("/", [LotoLeidsaController::class,"index"]) -> name("index");
+    Route::get("/agregar", [LotoLeidsaController::class,"create"])->name("agregar");
+    Route::post("/store", [LotoLeidsaController::class,"store"])->name("store");
+    Route::get("/editar/{lotteryResult}", [LotoLeidsaController::class,"edit"])->name("editar");
+    Route::put("/update/{lotteryResult}", [LotoLeidsaController::class,"update"])->name("update");
+    Route::delete("/delete/{lotteryResult}", [LotoLeidsaController::class,"destroy"]) -> name("delete");
+});
