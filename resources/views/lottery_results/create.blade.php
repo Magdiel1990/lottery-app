edf9137aef778a28b3c1f35e71baae6d4dd71afb
Magdiel Castillo <magdielmagdiel1@gmail.com>
2025-08-06 17:27:49 -0400

    correccion margin form create

--- resources/views/lottery_results/create.blade.php
+++ resources/views/lottery_results/create.blade.php
@@ -1,7 +1,7 @@
 @extends("layouts.app")
 
 @section("content")
-<div class="container mt-4">
+<div class="container mt-5">
     <div class="card shadow-sm w-50 mx-auto">
         <div class="card-header bg-primary text-white">
             <h5 class="mb-0"><i class="bi bi-pencil-square me-2"></i> Agregar nuevo resultado</h5>
