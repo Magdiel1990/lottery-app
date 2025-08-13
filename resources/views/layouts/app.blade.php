f53b4632ae6eccf4ffeb17792b185de22b099103
Magdiel <magdielmagdiel1@gmail.com>
2025-08-13 09:35:11 -0400

    arreglo de links de bootstrap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,13 +10,10 @@
         <!-- Bootstrap Icons -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
 
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-
         <!-- Estilo CSS-->
         <link rel="stylesheet" href="{{ asset('css/estilos.css') }}">
 
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-
         <title>Lottap</title>
     </head>
     <body>
