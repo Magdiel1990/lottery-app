bf2238b216bf3095d34fb06cc2037b9cba351719
Magdiel <magdielmagdiel1@gmail.com>
2025-08-13 09:25:20 -0400

    comentario de secciones que se trabajaran

--- resources/views/analize/index.blade.php
+++ resources/views/analize/index.blade.php
@@ -1,6 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-
+    <!-- 
+       1) Seccion para probar si el numero se puede jugar
+       2) Seccion donde se mostrara las estadisticas en tiempo real de esa loteria
+       3) Seccion para la probabilidad de ganarla
+       4) Seccion para generar un numero aleatorio que tenga probabilidad de salir (porque cumple con los patrones)  
+       5) Seccion para generar un numero aleatorio que tenga probabilidad de salir (porque no cumple con los patrones)  
+        6) Seccion para mostrar la grafica de los numeros que han salido
+        7) Sección de tendencias (muestra los numeros o combinaciones de numeros calientes y frios)
+        8) Sección de análisis inverso (Entras un numero o mas numeros y te dice cuando salio por ultima vez)
+        9) Sección de simulación de jugadas (pone a prueba la jugada a ver la probabilidad de salir)
+       -->
 
 @endsection
